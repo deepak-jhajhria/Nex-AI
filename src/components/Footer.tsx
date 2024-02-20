ff3779e1422260a0d40035419c619701a87369b4
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-02-20 10:05:01 +0530

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,21 @@ import { Ellipse, FacebookIcon, TelegramIcon, TwitterIcon } from "./Icons";
 
 export const Footer = () => {
   return (
-    <footer className="w-full sm:min-h-[492px] bg-[url(./assets/images/footerbg.webp)] bg-center bg-cover bg-no-repeat mt-[89px] relative overflow-x-clip">
+    <footer className="w-full sm:min-h-[492px] bg-[url(./assets/images/footerbg.webp)] bg-center bg-cover bg-no-repeat mt-8 md:mt-12 lg:mt-[89px] relative overflow-x-clip">
       <div className="container max-w-[1140px] pt-[30px]">
-        <div className="grid grid-cols-1 md:grid-cols-3 pt-8 sm:pt-16 md:pt-[90px] lg:pt-[150px]">
+        <div className="grid grid-cols-1 md:grid-cols-3 sm:pt-16 md:pt-[90px] lg:pt-[150px]">
           <div className="flex flex-col md:col-span-2 max-w-[356px]">
             <p className="text-sm font-bold text-white sm:text-base font-Recharge opacity-70">
               Nullam viverra natoque amet et sed scelerisque viverra fringilla
               platea. Ullamcorper.
             </p>
-            <div className="flex items-center gap-3 mt-3 sm:mt-5">
+            <div className="flex items-center gap-3 mt-4 sm:mt-5">
               <a className="duration-300 hover:-translate-y-2" href="http://facebook.com" target="_blank" rel="noopener noreferrer"><span><FacebookIcon /></span></a>
               <a className="duration-300 hover:-translate-y-2" href="http://Twitter.com" target="_blank" rel="noopener noreferrer"><span><TwitterIcon /></span></a>
               <a className="duration-300 hover:-translate-y-2" href="http://Telegram.com" target="_blank" rel="noopener noreferrer"><span><TelegramIcon /></span></a>
             </div>
           </div>
-          <div className="flex justify-between mt-7 xs:mt-10 md:-ml-10 md:-mt-9 lg:ml-0">
+          <div className="flex justify-between mt-9 xs:mt-10 md:-ml-10 md:-mt-9 lg:ml-0">
             <ul className="flex flex-col gap-2 sm:gap-[14px]">
               <li className="text-base font-bold text-white sm:text-lg font-Recharge">Quick links</li>
               <li><a className="text-sm font-bold text-white sm:text-base font-Recharge opacity-70 duration-300 hover:opacity-100 hover:text-[#02CDCF]" href="#home">Home</a></li>
